92153322cf64165857e6cfbe9133c2982932bb38
René Weiser <rene.weiser@uni-weimar.de>
2020-06-30 14:39:06 +0200

    Edited content and renamed it to posts.js

---
+++ content/posts.js
@@ -0,0 +1,35 @@
+const data = [
+  {
+    text: "Die Off-Location befindet sich in denkmalgeschützten Räumen aus dem Jahr 1886 und sind im Originalzustand.",
+    color: "beige",
+  },
+  {
+    image: "content/images/schweizer5-1.jpg",
+    color: "red",
+  },
+  {
+    text: "Sie beeindruckt durch holzvertäfelte Räume und Deckenmalereien und Matthäus Keufel, der unter anderem auc die Alte Open und das Palmengarten Gesellschaftshaus mit Bildern ausstattete.",
+    image: "content/images/schweizer5-2.jpg",
+  },
+  {
+    text: "Die Räumlichkeiten eignen sich für Dinners mit bis zu 60 Personen, Salonkonzerten, Lesungen und Vorträgen mit bis zu 80 Personen, sowie Stehempfängen und Veranstaltungen bis zu 99 Personen.",
+    image: "content/images/schweizer5-3.jpg",
+    color: "blue",
+  },
+  {
+    image: "content/images/schweizer5-4.jpg",
+    color: "beige",
+  },
+  {
+    text: "Ein Küchenbereich als Standort für externes Catering ist vorhanden.",
+    image: "content/images/schweizer5-5.jpg",
+    color: "red"
+  },
+  {
+    image: "content/images/schweizer5-6.jpg",
+    color: "blue",
+  }
+];
+export default {
+  data,
+};
