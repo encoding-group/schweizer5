e4affffda58d1e240eec9861a4796cf10a3ce934
René Weiser <reneweiser@Renes-MacBook-Pro.local>
2020-06-30 13:58:14 +0200

    Created constructor methods for sections

--- content/index.js
+++ content/index.js
@@ -1,27 +1,31 @@
-const index = [
-	{
-		text: 'Section 1',
-		image: 'images/image-1.jpg',
-		color: '#00f'
-	},
-	{
-		text: 'Section 2',
-		image: 'images/image-2.jpg',
-		color: '#0ff'
-	},
-	{
-		text: 'Section 3',
-		image: 'images/image-3.jpg',
-		color: '#ff0'
-	},
-	{
-		text: 'Section 4',
-		image: 'images/image-4.jpg',
-		color: '#f0f'
-	},
-	{
-		text: 'Section 5',
-		image: 'images/image-5.jpg',
-		color: '#0f0'
-	},
-];
+const data = 
+	[
+		{
+			text: 'Section 1',
+			image: 'images/image-1.jpg',
+			color: 'beige'
+		},
+		{
+			text: 'Section 2',
+			image: 'images/image-2.jpg',
+			color: 'blue'
+		},
+		{
+			text: 'Section 3',
+			image: 'images/image-3.jpg',
+			color: 'red'
+		},
+		{
+			text: 'Section 4',
+			image: 'images/image-4.jpg',
+			color: 'beige'
+		},
+		{
+			text: 'Section 5',
+			image: 'images/image-5.jpg',
+			color: 'red'
+		},
+	];
+export default {
+	data
+}
