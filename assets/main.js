16a8a08b776c577b19811acd3244c45e6d92d350
moritzebeling <git@moritzebeling.com>
2020-06-30 18:52:21 +0200

    rerun section resize on window resize

--- assets/main.js
+++ assets/main.js
@@ -3,3 +3,12 @@ import ImageParentResizer from "../assets/image-parent-resizer.js";
 
 PostRenderer.renderPosts();
 ImageParentResizer.resize();
+
+let _onResizeTimeout;
+window.onresize = function(){
+  clearTimeout(_onResizeTimeout);
+  _onResizeTimeout = setTimeout(function(){
+		console.log('resize');
+		ImageParentResizer.resize();
+  }, 150);
+};
