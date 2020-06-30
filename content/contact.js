72ef6268186ba722fdb0c16f5ea0f7c57893d5d4
moritzebeling <git@moritzebeling.com>
2020-06-30 13:38:14 +0200

    hardcode contact footer

--- content/contact.js
+++ content/contact.js
@@ -1,7 +1,7 @@
 const contact = {
 	headline: 'Rental Request',
 	name: ['Franci Rotariu','General Manager'],
-	address: ['Schweizer Strasse 5','60594 Frankfurt am Main']
-	tel: '+ 49 173 965 83 94'
+	address: ['Schweizer Strasse 5','60594 Frankfurt am Main'],
+	tel: '+49 173 9658394',
 	email: 'xyz@example.com'
 };
