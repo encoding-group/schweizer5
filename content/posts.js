dcc1d83d5d8ba109aeda85a2faf327ad18929055
René Weiser <rene.weiser@uni-weimar.de>
2020-06-30 15:31:34 +0200

    Added image alt text to the post object

--- content/posts.js
+++ content/posts.js
@@ -5,28 +5,34 @@ const posts = [
   },
   {
     image: "content/images/schweizer5-1.jpg",
+    imageText: "",
     color: "red",
   },
   {
     text: "Sie beeindruckt durch holzvertäfelte Räume und Deckenmalereien und Matthäus Keufel, der unter anderem auc die Alte Open und das Palmengarten Gesellschaftshaus mit Bildern ausstattete.",
     image: "content/images/schweizer5-2.jpg",
+    imageText: "",
   },
   {
     text: "Die Räumlichkeiten eignen sich für Dinners mit bis zu 60 Personen, Salonkonzerten, Lesungen und Vorträgen mit bis zu 80 Personen, sowie Stehempfängen und Veranstaltungen bis zu 99 Personen.",
     image: "content/images/schweizer5-3.jpg",
+    imageText: "",
     color: "blue",
   },
   {
     image: "content/images/schweizer5-4.jpg",
+    imageText: "",
     color: "beige",
   },
   {
     text: "Ein Küchenbereich als Standort für externes Catering ist vorhanden.",
     image: "content/images/schweizer5-5.jpg",
+    imageText: "",
     color: "red"
   },
   {
     image: "content/images/schweizer5-6.jpg",
+    imageText: "",
     color: "blue",
   }
 ];
