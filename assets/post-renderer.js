260a4caf5dd3fd45db08092b5ae953c032b843e7
René Weiser <rene.weiser@uni-weimar.de>
2020-06-30 17:47:04 +0200

    Changed post renderer to be compatible with new post object structure

--- assets/post-renderer.js
+++ assets/post-renderer.js
@@ -17,26 +17,20 @@ const makeColor = function (colorName) {
   return color;
 };
 
-const makeImage = function (imagePath, imageText) {
+const makeImage = function ({ path, alt, width, height }) {
   const wrapper = document.createElement("div");
   wrapper.className = "image";
 
   const figure = document.createElement("figure");
   const image = document.createElement("img");
-  image.setAttribute("src", imagePath);
-  // console.log(generateSrcSetString(imagePath, [getImageWidthFromName(imagePath), 1000, 500]));
-  image.setAttribute("srcset", generateSrcSetString(imagePath, [getImageWidthFromName(imagePath), 1000, 500]));
-  image.setAttribute("alt", imageText);
+  image.setAttribute("src", path);
+  image.setAttribute("srcset", generateSrcSetString(path, [width, 1000, 500]));
+  image.setAttribute("alt", alt);
 
   figure.appendChild(image);
   wrapper.appendChild(figure);
   return wrapper;
 };
-
-const getImageWidthFromName = function (imagePath) {
-  let chunks = imagePath.split("-");
-  return chunks[chunks.length - 1].split(".")[0];
-}
 
 const generateSrcSetString = function (imagePath, sizes) {
   let srcSetStrings = [];
@@ -68,7 +62,7 @@ Content.posts.forEach((post) => {
   }
 
   if ("image" in post) {
-    section.appendChild(makeImage(post.image, post.imageText));
+    section.appendChild(makeImage(post.image));
   }
 
   if ("color" in post) {
