0ed006e24966a92813987bceb25d0f713a36575f
René Weiser <rene.weiser@uni-weimar.de>
2020-07-01 07:07:30 +0200

    Changed content formatting back to js objects and exported the data directly

--- content/contact.js
+++ content/contact.js
@@ -1,7 +1,11 @@
-const contact = {
-	headline: 'Rental Request',
-	name: ['Franci Rotariu','General Manager'],
-	address: ['Schweizer Strasse 5','60594 Frankfurt am Main'],
-	tel: '+49 173 9658394',
-	email: 'xyz@example.com'
+export default {
+  businessName: "Rental Request",
+  name: "Franci Rotariu",
+  position: "General Manager",
+  address: {
+    street: "Schweizer Strasse 5",
+    city: "60594 Frankfurt am Main",
+  },
+  tel: "+49 173 9658394",
+  email: "xyz@example.com",
 };
--- content/posts.js
+++ content/posts.js
@@ -1,66 +1,65 @@
-const posts = [
+export default [
   {
-    "text": "Die Off-Location befindet sich in denkmalgeschützten Räumen aus dem Jahr 1886 und sind im Originalzustand.",
-    "color": "beige"
+    text:
+      "Die Off-Location befindet sich in denkmalgeschützten Räumen aus dem Jahr 1886 und sind im Originalzustand.",
+    color: "beige",
   },
   {
-    "image": {
-      "path": "content/images/schweizer5-1-1786.jpg",
-      "alt": "",
-      "width": 1786,
-      "height": 2547
-    }
-  },
-  {
-    "text": "Sie beeindruckt durch holzvertäfelte Räume und Deckenmalereien und Matthäus Keufel, der unter anderem auc die Alte Open und das Palmengarten Gesellschaftshaus mit Bildern ausstattete.",
-    "image": {
-      "path": "content/images/schweizer5-2-2288.jpg",
-      "alt": "",
-      "width": 2288,
-      "height": 1522
+    image: {
+      path: "content/images/schweizer5-1-1786.jpg",
+      alt: "",
+      width: 1786,
+      height: 2547,
     },
-    "color": "red"
   },
   {
-    "text": "Die Räumlichkeiten eignen sich für Dinners mit bis zu 60 Personen, Salonkonzerten, Lesungen und Vorträgen mit bis zu 80 Personen, sowie Stehempfängen und Veranstaltungen bis zu 99 Personen.",
-    "image": {
-      "path": "content/images/schweizer5-3-2035.jpg",
-      "alt": "",
-      "width": 2035,
-      "height": 3058
+    text:
+      "Sie beeindruckt durch holzvertäfelte Räume und Deckenmalereien und Matthäus Keufel, der unter anderem auc die Alte Open und das Palmengarten Gesellschaftshaus mit Bildern ausstattete.",
+    image: {
+      path: "content/images/schweizer5-2-2288.jpg",
+      alt: "",
+      width: 2288,
+      height: 1522,
     },
-    "color": "blue"
+    color: "red",
   },
   {
-    "image": {
-      "path": "content/images/schweizer5-4-2035.jpg",
-      "alt": "",
-      "width": 2035,
-      "height": 1521
+    text:
+      "Die Räumlichkeiten eignen sich für Dinners mit bis zu 60 Personen, Salonkonzerten, Lesungen und Vorträgen mit bis zu 80 Personen, sowie Stehempfängen und Veranstaltungen bis zu 99 Personen.",
+    image: {
+      path: "content/images/schweizer5-3-2035.jpg",
+      alt: "",
+      width: 2035,
+      height: 3058,
     },
-    "color": "beige"
+    color: "blue",
   },
   {
-    "text": "Ein Küchenbereich als Standort für externes Catering ist vorhanden.",
-    "image": {
-      "path": "content/images/schweizer5-5-2035.jpg",
-      "alt": "",
-      "width": 2035,
-      "height": 3044
+    image: {
+      path: "content/images/schweizer5-4-2035.jpg",
+      alt: "",
+      width: 2035,
+      height: 1521,
     },
-    "color": "red"
+    color: "beige",
   },
   {
-    "image": {
-      "path": "content/images/schweizer5-6-1783.jpg",
-      "alt": "",
-      "width": 1783,
-      "height": 2812
+    text: "Ein Küchenbereich als Standort für externes Catering ist vorhanden.",
+    image: {
+      path: "content/images/schweizer5-5-2035.jpg",
+      alt: "",
+      width: 2035,
+      height: 3044,
     },
-    "color": "blue"
-  }
+    color: "red",
+  },
+  {
+    image: {
+      path: "content/images/schweizer5-6-1783.jpg",
+      alt: "",
+      width: 1783,
+      height: 2812,
+    },
+    color: "blue",
+  },
 ];
-
-export default {
-  posts,
-};
