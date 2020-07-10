31ff959b75d10a065e8f9e50787928d98aebc791
René Weiser <rene.weiser@uni-weimar.de>
2020-07-11 00:03:59 +0200

    Cleaned up project

--- src/js/post-loader.js
+++ src/js/post-loader.js
@@ -5,17 +5,11 @@ const loadContentSync = () => {
   let dir = path.resolve(__dirname, '../../content');
   let posts = {};
 
-  fs.readdirSync(dir).forEach((file, index) => {
-    // text-1-en
+  fs.readdirSync(dir).forEach((file) => {
     let fileName = file.split('.')[0].split('-').join('');
-    // if (posts[fileNameFragments[1] == undefined]) {
-    //   posts[fileNameFragments[1]] = {};
-    // }
-    // if (posts[fileNameFragments[1]][fileNameFragments[2]] == undefined) {
-    //   posts[fileNameFragments[1]][fileNameFragments[2]] = {};
-    // }
     posts[fileName] = fs.readFileSync(`${dir}/${file}`, { encoding: 'utf8' });
   });
+
   return posts;
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const content = require('./src/js/post-loader')();
 const md = require('markdown-it')();
-texts.facts = md.render(texts.facts);
+
+content.facts = md.render(content.facts);
 
 module.exports = {
   entry: './src/index.js',
@@ -12,13 +13,9 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.html$/i,
-      //   use: 'html-loader'
-      // },
       {
-        test:/\.hbs$/i,
-        loader: 'handlebars-loader'
+        test: /\.hbs$/,
+        loader: 'handlebars-loader',
       },
       {
         test: /\.s[ac]ss$/,
@@ -31,19 +28,17 @@ module.exports = {
           name: '[path][name].[ext]',
         },
       },
-      {
-        test: /\.md$/,
-        loader: 'frontmatter-markdown-loader',
-      },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    hash: true,
-    title: 'Test title',
-    template: './src/index.hbs',
-    filename: './index.html',
-    templateParameters: {
-      content
-    }
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Test title',
+      template: './src/index.hbs',
+      filename: './index.html',
+      templateParameters: {
+        content,
+      },
+    }),
+  ],
 };
